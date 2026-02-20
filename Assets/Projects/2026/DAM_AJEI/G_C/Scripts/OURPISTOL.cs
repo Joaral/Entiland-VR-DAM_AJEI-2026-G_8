ce8c358f1ba459dd5fc13bdf223bccc5c89d2bdb
Joaral <joan.ariza@estudiant.enti.cat>
2026-02-20 20:48:45 +0100

    primer commit

---
+++ Assets/Projects/2026/DAM_AJEI/G_C/Scripts/OURPISTOL.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Autohand.Demo
+{
+
+    public class OURPISTOL : MonoBehaviour
+    {
+        public bool loaded = false;
+        public Rigidbody body;
+
+        public Transform barrelTip;
+        public float hitPower = 1;
+        public float recoilPower = 1;
+        public float range = 100;
+        public LayerMask layer;
+
+        public AudioClip shootSound;
+        public float shootVolume = 1f;
+
+        private void Start()
+        {
+            if (body == null && GetComponent<Rigidbody>() != null)
+                body = GetComponent<Rigidbody>();
+        }
+
+        public void Shoot()
+        {
+            if (!loaded)
+            {
+                return;
+            }
+
+            //Play the audio sound
+            if (shootSound)
+                AudioSource.PlayClipAtPoint(shootSound, transform.position, shootVolume);
+
+            RaycastHit hit;
+            if (Physics.Raycast(barrelTip.position, barrelTip.forward, out hit, range, layer))
+            {
+                var hitBody = hit.transform.GetComponent<Rigidbody>();
+                if (hitBody != null)
+                {
+                    Debug.DrawRay(barrelTip.position, (hit.point - barrelTip.position), Color.green, 5);
+                    hitBody.GetComponent<Smash>()?.DoSmash();
+                    hitBody.AddForceAtPosition((hit.point - barrelTip.position).normalized * hitPower * 10, hit.point, ForceMode.Impulse);
+                }
+            }
+            else
+                Debug.DrawRay(barrelTip.position, barrelTip.forward * range, Color.red, 1);
+
+            body.AddForce(barrelTip.transform.up * recoilPower * 5, ForceMode.Impulse);
+        }
+    }
+}
